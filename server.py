4eb99a3ebbaeaa0bce8b49a83830b2533fc6f53c
cappadavide <davidex.somma@gmail.com>
2021-12-21 19:36:38 +0100

    Aggiunti files

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from OpenSSL import SSL
 context = SSL.Context(method = SSL.TLSv1_1_METHOD)
 context.load_verify_locations(cafile=None,capath="../certs")
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.bind(('192.168.1.112', 4433))
+sock.bind(('127.0.0.1', 4433))
 sock.listen(5)
 conn = SSL.Connection(context,socket=sock)
 print("Sono in ascolto...\n")
