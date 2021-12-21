cfacba8d2f4c19313cb75ddeb879a53f9ec15970
cappadavide <davidex.somma@gmail.com>
2021-12-21 19:58:33 +0100

    Aggiunti files

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import datetime
 from OpenSSL import SSL
 
-context = SSL.Context(method = SSL.TLSv1_1_METHOD)
+context = SSL.Context(method = SSL.TLSv1_METHOD)
 context.load_verify_locations(cafile=None,capath="../certs")
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 conn = SSL.Connection(context,socket=sock)
