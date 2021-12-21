905e1165a0fc63fafeb9cc6cb0ff7adb4be7dfcc
cappadavide <davidex.somma@gmail.com>
2021-12-21 19:33:51 +0100

    Aggiunti files

--- server.py
+++ server.py
@@ -8,5 +8,6 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.bind(('192.168.1.112', 4433))
 sock.listen(5)
 conn = SSL.Connection(context,socket=sock)
-conn, addr = conn.accept()
 print("Sono in ascolto...\n")
+conn, addr = conn.accept()
+
