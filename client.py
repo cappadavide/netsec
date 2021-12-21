3056259f8f25d490a95205679931a9365b1823f8
cappadavide <davidex.somma@gmail.com>
2021-12-21 18:01:05 +0100

    Aggiunti files

---
+++ client.py
@@ -0,0 +1,180 @@
+import ssl
+import socket
+import datetime
+import hashlib
+from OpenSSL import SSL
+
+from cryptography import x509
+from cryptography import exceptions
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives._serialization import Encoding, PublicFormat
+
+
+
+def parseName(name: x509.Name):
+    
+    certstr=""
+    
+    for i in name:
+        
+        if i.rfc4514_attribute_name == "CN":
+            certstr += f"\tCommon Name: {i.value}\n"
+            
+        elif i.rfc4514_attribute_name == "C":
+            certstr += f"\tCountry:{i.value}\n"
+            
+        elif i.rfc4514_attribute_name == "ST":
+            certstr += f"\tState:{i.value}\n"
+            
+        elif i.rfc4514_attribute_name == "L":
+            certstr += f"\tLocality: {i.value}\n"
+            
+        elif i.rfc4514_attribute_name == "O":
+            certstr += f"\tOrganization:{i.value}\n"
+            
+    return certstr
+
+def parseExtensions(ext: x509.Extensions):
+    
+    certstr = ""
+    
+    for e in ext:
+        
+        if isinstance(e.value,x509.BasicConstraints):
+            
+            stringa = f"\n\tPath Length:{e.value.path_length}" if e.value.path_length is not None else ""
+            
+            certstr += f"\tBasic Constraint:\n\t\tCA:{e.value.ca}{stringa}, critical={e.critical}\n"
+            
+        elif isinstance(e.value,x509.SubjectAlternativeName):
+            
+            certstr += "\tSubject Alternative Name:\n\t"
+            
+            for types in [x509.DNSName,x509.RFC822Name,x509.DirectoryName,x509.IPAddress,x509.UniformResourceIdentifier,x509.RegisteredID,x509.OtherName]:
+                
+                value = e.value.get_values_for_type(types)
+                
+                if value:
+                    
+                    certstr+=f"\t{types.__name__}:{value}\n"
+
+            certstr +=f"\t\tcritical={e.critical}\n"
+
+    return certstr
+
+def parsingString(cert : x509.Certificate):
+    
+    certstr = ""
+    certstr += "Subject:\n"
+    certstr += parseName(cert.subject)
+    certstr += "Issuer:\n"
+    certstr += parseName(cert.issuer)
+    
+    certstr += f"Certificate Version:\n\t{cert.version.value}\n"
+    certstr += f"""Validity Period:\n\t{cert.not_valid_before} - {cert.not_valid_after}\n"""
+    certstr += f"Serial Number:\n\t{cert.serial_number}\n"
+    certstr += f"Public Key:\n\t{cert.public_key().public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)}\n"
+    certstr += f"Certificate signed with:\n\t{cert.signature_algorithm_oid._name} algorithm\n"
+    certstr += f"Signature:\n\t{cert.signature}\n"
+    certstr += f"Extensions:\n{parseExtensions(cert.extensions)}"
+
+    return certstr
+
+
+def checkDigitalSignature(certificates):
+    
+    for i in range(len(certificates)):
+        
+        try:
+            if certificates[i].subject == certificates[i].issuer:
+
+                certificates[i].public_key().verify(signature=certificates[i].signature,data=certificates[i].tbs_certificate_bytes,padding=padding.PKCS1v15(),algorithm=certificates[i].signature_hash_algorithm)
+
+            else:
+
+                certificates[i+1].public_key().verify(signature=certificates[i].signature,data=certificates[i].tbs_certificate_bytes,padding=padding.PKCS1v15(),algorithm=certificates[i].signature_hash_algorithm)
+
+        except exceptions.InvalidSignature as e:
+
+            return False
+
+    return True
+
+
+def checkCertValidity(certificate: x509.Certificate):
+    
+        current_date = datetime.datetime.now()
+
+        return True if current_date >= certificate.not_valid_before and current_date <= certificate.not_valid_after else False
+
+def checkIfRootTrustAnchor(certificates,trustedCertPath):
+    
+    cert = x509.load_pem_x509_certificate(open(trustedCertPath,"r").read().encode('utf-8'))
+
+    return True if mycert == certificates[-1] else False
+
+def main():
+
+    certificates = []
+    hostname = '192.168.1.112'
+    port = 4433
+    trustedCertPath = "../cert_root.pem"
+
+    #set ssl version and context
+    context = SSL.Context(method=SSL.TLSv1_METHOD)
+
+    #verify the chain certificate root
+    context.set_verify(SSL.VERIFY_FAIL_IF_NO_PEER_CERT or SSL.VERIFY_PEER)
+    context.load_verify_locations(cafile="../cert_root.pem")
+
+    #create connection between client and server
+    conn = SSL.Connection(context, socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    conn.settimeout(5)
+    conn.connect((hostname, port))
+    conn.setblocking(1)
+    conn.do_handshake()
+    conn.set_tlsext_host_name(hostname.encode())
+    
+    for cert in conn.get_peer_cert_chain():
+        
+        certificates.append(cert.to_cryptography())
+
+    #basic certification
+    for cert in certificates:
+        
+        print(checkCertValidity(cert))
+
+    print(checkIfRootTrustAnchor(certificates,trustedCertPath))
+    print(checkDigitalSignature(certificates))
+
+    print("\nCERTIFICATE PARSING\n")
+    for cert in certificates:
+
+        parsingString(cert)
+        print("\n")
+
+    conn.close()
+
+
+"""
+def parsingDict(cert: x509.Certificate):
+    certdict = {'subject': parseName(cert.subject),'issuer': parseName(cert.issuer),
+            'version': cert.version.value, 'validityPeriod':[cert.not_valid_before,cert.not_valid_after],
+            'sn': cert.serial_number,'publickey':cert.public_key().public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo),
+            'signalgorithm':cert.signature_algorithm_oid._name,'signature':cert.signature,'extensions':{},'encoded':"cacca"
+            }
+    for e in cert.extensions:
+        if isinstance(e.value,x509.BasicConstraints):
+            certdict["extensions"]["basicConstraint"] = {'ca':e.value.ca,'pathLen':e.value.path_length,'critical':e.critical}
+        elif isinstance(e.value,x509.SubjectAlternativeName):
+            certdict["extensions"]["subjectAltName"]={}
+            for types in [x509.DNSName,x509.RFC822Name,x509.DirectoryName,x509.IPAddress,x509.UniformResourceIdentifier,x509.RegisteredID,x509.OtherName]:
+                value = e.value.get_values_for_type(types)
+                if value:
+                    certdict["extensions"]["subjectAltName"][types.__name__] = value
+            certdict["extensions"]["subjectAltName"]["critical"] = e.critical
+    return certdict
+"""
---
+++ server.py
@@ -0,0 +1,152 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+import datetime
+
+
+def generate_private_rsakey(i,chain_len):
+    
+    #Generate our key
+    key = rsa.generate_private_key(
+        
+        public_exponent=65537,
+        key_size=2048,
+    )
+
+    if i == 1: name = "privatekey_root"
+    elif i>1 and i<chain_len: name = str("privatekey_"+i)
+    else: name = "privatekey_server"
+    
+    #Write our key to disk for safe keeping
+    with open(str("../"+name+".pem"), "wb") as f:
+        
+        f.write(key.private_bytes(
+            
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.BestAvailableEncryption(b"passphrase"),
+        ))
+        
+    return key
+
+def generate_certificate(i,cert_chain,private_key,chain_len,params,issuer_list):
+
+    #params
+    subject = x509.Name([
+        
+        x509.NameAttribute(NameOID.COUNTRY_NAME, params['COUNTRY_NAME']),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, params['STATE_OR_PROVINCE_NAME']),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, params['LOCALITY_NAME']),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, params['ORGANIZATION_NAME']),
+        x509.NameAttribute(NameOID.COMMON_NAME, params['COMMON_NAME']),
+    ])
+    issuer_list[i]=subject
+    
+    #root case
+    if i == 1:
+        
+        cert = x509.CertificateBuilder().subject_name(
+            subject
+        ).issuer_name(
+            issuer_list[i]
+        ).public_key(
+            private_key[i].public_key()
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            datetime.datetime.utcnow()
+        ).not_valid_after(
+            #Our certificate will be valid for 10 days
+            datetime.datetime.utcnow() + datetime.timedelta(days=10)
+        ).add_extension(
+            x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+            critical=False,
+        ).add_extension(x509.BasicConstraints(ca=True, path_length=None),critical=True,
+        ).sign(private_key[i], hashes.SHA256())#Sign our certificate with our private key
+        
+    #intermediate CA        
+    elif i>1 and i<chain_len:
+
+        cert = x509.CertificateBuilder().subject_name(
+            subject
+        ).issuer_name(
+            issuer_list[i-1]
+        ).public_key(
+            private_key[i].public_key()
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            datetime.datetime.utcnow()
+        ).not_valid_after(
+            #Our certificate will be valid for 10 days
+            datetime.datetime.utcnow() + datetime.timedelta(days=10)
+        ).add_extension(
+            x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+            critical=False,
+        ).add_extension(x509.BasicConstraints(ca=True, path_length=None),critical=True,
+        ).sign(private_key[i-1], hashes.SHA256())#Sign our certificate with our private key
+
+    #leaf for server, without basic constraints ca=true
+    #last certificate
+    else:
+
+        cert = x509.CertificateBuilder().subject_name(
+            subject
+        ).issuer_name(
+            issuer_list[i-1]
+        ).public_key(
+            private_key[i].public_key()
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            datetime.datetime.utcnow()
+        ).not_valid_after(
+            #Our certificate will be valid for 10 days
+            datetime.datetime.utcnow() + datetime.timedelta(days=10)
+        ).add_extension(
+            x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+            critical=False,
+        ).sign(private_key[i-1], hashes.SHA256())#Sign our certificate with our private key
+
+
+    if i == 1: name = "cert_root"
+    elif i>1 and i<chain_len: name = str("cert_"+i)
+    else: name = "cert_server"
+
+    if name == "cert_server": path="../" #certificato del server non va hashato
+    else: path = "../certs/"
+    
+    #Write our certificate to disk for safe keeping
+    with open(str(path+name+".pem"), "wb") as f:
+        
+        f.write(cert.public_bytes(serialization.Encoding.PEM))
+     
+        
+    return cert   
+    
+def set_params(params):
+
+    params['COUNTRY_NAME'] = u"US"
+    params['STATE_OR_PROVINCE_NAME'] = u"California"
+    params['LOCALITY_NAME'] = u"San Francisco"
+    params['ORGANIZATION_NAME'] = u"My Company"
+    params['COMMON_NAME'] = u"mysite.com"
+
+    return params
+    
+def main():
+
+    params = {}
+    issuer_list = []
+    private_key = []
+    cert_chain = []
+    chain_len = 3
+
+    params = set_params(params)
+
+    for i in range(1,len+1):
+        
+        private_key[i] = generate_private_rsakey(i,chain_len)
+        cert_chain[i] = generate_certificate(i,cert_chain,private_key,chain_len,params,issuer_list)
