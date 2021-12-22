706333c13622072e0d04a9825c115beec491eefa
cappadavide <davidex.somma@gmail.com>
2021-12-22 16:40:53 +0100

    jdjdjd

--- server.py
+++ server.py
@@ -3,13 +3,13 @@ import datetime
 from OpenSSL import SSL
 
 context = SSL.Context(method = SSL.TLSv1_METHOD)
-#context.set_verify(SSL.VERIFY_NONE)
-
-#context.load_verify_locations(cafile=None,capath="../certs")
-#context.use_certificate_file("../cert_server.pem")
-#context.use_privatekey_file("../privatekey_server.pem")
+context.set_verify(SSL.VERIFY_PEER)
+context.load_verify_locations(cafile=None,capath="../certs")
+context.use_certificate_file("../cert_server.pem")
+context.use_privatekey_file("../privatekey_server.pem")
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 conn = SSL.Connection(context,socket=sock)
+
 conn.bind(('', 4433))
 conn.listen(5)
 print("Sono in ascolto...\n")
