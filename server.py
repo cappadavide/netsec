b4f36d9ab2b39a45830790f8ae5e4fca26c278cd
cappadavide <davidex.somma@gmail.com>
2021-12-22 17:19:19 +0100

    jdjdjd

--- server.py
+++ server.py
@@ -4,10 +4,11 @@ from OpenSSL import SSL
 
 
 context = SSL.Context(method = SSL.TLSv1_METHOD)
-context.set_verify(SSL.VERIFY_PEER and SSL.VERIFY_FAIL_IF_NO_PEER_CERT)
+context.set_verify(SSL.VERIFY_PEER or SSL.VERIFY_FAIL_IF_NO_PEER_CERT or SSL.VERIFY_CLIENT_ONCE)
 context.load_verify_locations(cafile=None,capath="../certs")
 context.use_certificate_file("../cert_server.pem")
 context.use_privatekey_file("../privatekey_server.pem")
+
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 conn = SSL.Connection(context,socket=sock)
