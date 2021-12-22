e82330df32e1fc8b1202949070d017d5d33159a0
cappadavide <davidex.somma@gmail.com>
2021-12-22 17:11:40 +0100

    jdjdjd

--- server.py
+++ server.py
@@ -4,11 +4,12 @@ from OpenSSL import SSL
 
 
 context = SSL.Context(method = SSL.TLSv1_METHOD)
-context.set_verify(SSL.VERIFY_PEER)
+context.set_verify(SSL.VERIFY_FAIL_IF_NO_PEER_CERT)
 context.load_verify_locations(cafile=None,capath="../certs")
 context.use_certificate_file("../cert_server.pem")
 context.use_privatekey_file("../privatekey_server.pem")
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 conn = SSL.Connection(context,socket=sock)
 conn.bind(('', 4433))
 conn.listen(5)
@@ -17,8 +18,7 @@ print("Sono in ascolto...\n")
 server, addr = conn.accept()
 
 server_ssl = SSL.Connection(context, server)
-
 server_ssl.set_accept_state()
 server_ssl.do_handshake()
-print(server.get_peer_certificate().to_cryptography())
+
 server.close()
