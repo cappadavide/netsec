394d3e9c349e9e180dedd8c76ab28ca14f49b095
cappadavide <davidex.somma@gmail.com>
2021-12-26 19:22:52 +0100

    blabla

---
+++ Client.java
@@ -0,0 +1,65 @@
+import java.net.*;
+import java.security.*;
+import java.io.*;
+import javax.net.ssl.*;
+import java.nio.charset.StandardCharsets;
+public class Client{
+
+    public static void main(String[] args) {
+
+        File file = new File("../client.pkcs12");
+        File fileks = new File("../truststore.ks");
+        try
+        {
+            InputStream in = new FileInputStream(file);
+            InputStream in2 = new FileInputStream(fileks);
+            KeyStore keystore = KeyStore.getInstance("PKCS12");
+            String pw = "passphrase1";
+            keystore.load(in,pw.toCharArray());
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(keystore, pw.toCharArray());
+            KeyStore keystore_ca = KeyStore.getInstance("JKS");
+            String pwks = "keystore";
+            keystore_ca.load(in2, pwks.toCharArray());
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keystore_ca);
+            Socket sock = new Socket("192.168.1.112", 4433);
+            
+            InputStream inn = sock.getInputStream();
+            OutputStream outt = sock.getOutputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inn));
+            PrintWriter pwr  = new PrintWriter(new OutputStreamWriter(outt), true);
+            if (inn == null || outt == null) {
+                System.out.println("Failed to open streams to socket.");
+            }
+            System.out.println(br.readLine());
+            pwr.println("HELO tester.com");
+            System.out.println(br.readLine());
+            pwr.println("STARTTLS");
+            System.out.println(br.readLine());
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+            SSLSocketFactory ssf = sslContext.getSocketFactory();
+            //SSLSocket ssock = (SSLSocket) ssf.createSocket("192.168.1.112",4433);
+            SSLSocket ssock = (SSLSocket) ssf.createSocket(sock,"192.168.1.112", 4433, false);
+            System.out.print("Sono qui yeee1\n");
+            ssock.startHandshake();
+            System.out.print("Sono qui yeee\n");
+            pwr.println("EHLO tester.com");
+            System.out.println(br.readLine());
+
+            
+            
+
+            sock.close();  
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+
+        
+
+    }
+}
