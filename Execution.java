3f0b2e47cbf4f0443c9e643826f3258083551d39
cappadavide <davidex.somma@gmail.com>
2021-12-28 17:52:36 +0100

    tls

---
+++ Execution.java
@@ -0,0 +1,181 @@
+import java.net.*;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.io.*;
+import java.util.Scanner;
+import javax.net.ssl.*;
+import java.util.Base64;
+import java.util.Date;
+
+class Client2{
+    private File trustStore;
+    private File keystore;
+    private File trustPEM;
+    private KeyManagerFactory kmf;
+    private TrustManagerFactory tmf;
+    private Socket sock;
+    
+    public Client2(String tPath,String kPath,String tPEMPath){
+        this.trustStore = new File(tPath);
+        this.keystore =  new File(kPath);
+        this.trustPEM = new File(tPEMPath);
+    }
+
+    private boolean loadCerts(){
+        try{
+            InputStream in = new FileInputStream(keystore);
+            InputStream in2 = new FileInputStream(trustStore);
+            KeyStore keystore = KeyStore.getInstance("PKCS12");
+            Scanner scan= new Scanner(System.in);
+            System.out.println("Inserire password keystore:");
+            String pw= scan.nextLine();
+            keystore.load(in,pw.toCharArray());
+            this.kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            this.kmf.init(keystore, pw.toCharArray());
+            KeyStore keystore_ca = KeyStore.getInstance("JKS");
+            System.out.println("Inserire password truststore:");
+            String pwks= scan.nextLine();
+            keystore_ca.load(in2, pwks.toCharArray());
+            this.tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            this.tmf.init(keystore_ca);
+            scan.close();
+        }
+        catch(Exception e){
+            return false;
+        }
+        return true;
+
+    }
+
+    public boolean connect(String ip,int port){
+        if (loadCerts()){
+            try {
+                this.sock = new Socket(ip, port);
+
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        else
+            return false;
+        
+        return true;
+    }
+
+    public boolean startSMTPClient(){
+        try {
+            InputStream inn = this.sock.getInputStream();
+            OutputStream outt = this.sock.getOutputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inn));
+            PrintWriter pwr  = new PrintWriter(new OutputStreamWriter(outt), true);
+            if (inn == null || outt == null) {
+                System.out.println("Failed to open streams to socket.");
+            }
+            System.out.println(br.readLine());
+            pwr.println("HELO tester.com");
+            System.out.println(br.readLine());
+
+            pwr.println("STARTTLS");
+            System.out.println(br.readLine());
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+            SSLSocketFactory ssf = sslContext.getSocketFactory();
+
+            SSLSocket ssock = (SSLSocket) ssf.createSocket(sock,"192.168.1.112", 4433, false);
+            ssock.setUseClientMode(true);
+
+            inn = ssock.getInputStream();
+            outt = ssock.getOutputStream();
+            br = new BufferedReader(new InputStreamReader(inn));
+            pwr  = new PrintWriter(new OutputStreamWriter(outt), true);
+            if (inn == null || outt == null) {
+                System.out.println("Failed to open streams to socket.");
+            }
+
+
+            ssock.startHandshake();
+
+            SSLSession session = ssock.getSession();
+
+            X509Certificate [] certificates = (X509Certificate []) session.getPeerCertificates();
+            for (Certificate i: certificates){
+                System.out.println(Client2.checkValidity((X509Certificate)i));
+            }
+            System.out.println("CheckRoot:"+Client2.checkIfRootTrustAnchor(certificates,(InputStream)new FileInputStream(this.trustPEM)));
+            System.out.println("CheckSig:"+Client2.checkDigitalSignature(certificates));
+
+            pwr.println("EHLO tester.com");
+            System.out.println(br.readLine());
+            pwr.println("AUTH LOGIN");
+            System.out.println(br.readLine());
+            pwr.println(Base64.getEncoder().encodeToString("user1".getBytes()));
+            System.out.println(br.readLine());
+            pwr.println(Base64.getEncoder().encodeToString("password1".getBytes()));
+            System.out.println(br.readLine());
+            
+            //EMAIL
+            pwr.println("MAIL FROM: <davi.somma@studenti.unina.it>");
+            System.out.println(br.readLine());
+            pwr.println("RCPT to: <i.tieri@studenti.unina.it>");
+            System.out.println(br.readLine());
+            pwr.println("DATA");
+            System.out.println(br.readLine());
+
+            pwr.println("Subject: Test!");
+            pwr.println("From: davi.somma@studenti.unina.it");
+            pwr.println("To: i.tieri@studenti.unina.it");
+            pwr.println("Ciao!");
+            pwr.println(".");
+            System.out.println(br.readLine());
+            pwr.println("QUIT");
+            System.out.println(br.readLine());
+            br.close();
+            pwr.close();
+            ssock.close();  
+            sock.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    
+    }
+
+    public static boolean checkValidity(X509Certificate c){
+        Date date=new Date();
+        return date.compareTo(c.getNotBefore()) >= 0 && date.compareTo(c.getNotAfter()) <=0;
+    }
+    public static boolean checkIfRootTrustAnchor(X509Certificate [] c,InputStream trustcert) throws Exception{
+        CertificateFactory fact = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) fact.generateCertificate(trustcert);
+        return cert.equals(c[c.length-1]);
+    }
+    public static boolean checkDigitalSignature(X509Certificate [] certificates){
+        for(int i=0; i<certificates.length;i++){
+            try {
+                if (certificates[i].getSubjectX500Principal().equals(certificates[i].getIssuerX500Principal())){
+                    certificates[i].verify(certificates[i].getPublicKey());
+                }
+                else{
+                    certificates[i].verify(certificates[i+1].getPublicKey());
+                }
+
+            } catch (Exception e) {
+                return false;
+            
+            }
+        }
+        return true;
+    }
+
+}
+
+public class Execution{
+    public static void main(String[] args) {
+        Client2 client = new Client2("../truststore.ks","../client.pkcs12","../certs/cert_root.pem");
+        client.connect("192.168.1.112",4433);
+        client.startSMTPClient();
+    }
+    
+}
